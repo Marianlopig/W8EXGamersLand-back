da88aee6da97103f2b58777640182ccc7ebc7540
Olga Tsybikova <tsy2701@gmail.com>
2022-05-23 16:14:22 +0200

    created register user controller

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,34 @@
+require("dotenv").config;
+const bcrypt = require("bcrypt");
+
 const userLogin = () => {};
 
+const userRegister = async (req, res, next) => {
+  const { name, username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const error = new Error();
+    error.code = 409;
+    error.message = "This user already exists...";
+
+    next(error);
+  }
+  try {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = { name, username, password: encryptedPassword };
+
+    const createdUser = await User.create(newUser);
+
+    res.status(201).json({ name });
+  } catch (error) {
+    error.code = 400;
+    error.message = "Wrong user data..";
+    next(error);
+  }
+};
+
 module.exports = {
+  userRegister,
   userLogin,
 };
