7559d3de7da6940df967db798cb8bd6842d7f7f6
Olga Tsybikova <tsy2701@gmail.com>
2022-05-24 00:11:57 +0200

    changes due to sonarfilter

--- src/controllers/gameControllers.js
+++ src/controllers/gameControllers.js
@@ -1,13 +1,5 @@
-const Game = require("../database/models/Game");
-
-const games = [
-  { id: 1, title: "Mario" },
-  { id: 2, title: "Hide and Seek" },
-];
-
 const getGames = async (req, res) => {
-  const games = await Game.find();
-  res.status(200).json({ games });
+  res.status(200).json();
 };
 
 module.exports = { getGames };
